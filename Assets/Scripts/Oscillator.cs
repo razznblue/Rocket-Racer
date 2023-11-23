854aa9047c471942e5cd7e7df421e16e8ada2fa5
razznblue <kaipojames12@gmail.com>
2023-11-23 12:35:42 -0600

    Add Oscillator to make objects move

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+
+    Vector3 startingPos;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0, 1)] float movementFactor;
+    [SerializeField] float period = 2f;
+
+    void Start()
+    {
+      startingPos = transform.position;
+    }
+
+    void Update()
+    {
+      float cycles = Time.time / period;
+      const float tau = Mathf.PI * 2; // Represents a full circle rotation with radians
+      float rawSinWave = Mathf.Sin(cycles * tau);
+      // Make sure value of rawSinWave is between 0 and 1
+      movementFactor = (rawSinWave + 1f) / 2;
+
+      Vector3 offset = movementVector * movementFactor;
+      transform.position = startingPos + offset;
+    }
+}
